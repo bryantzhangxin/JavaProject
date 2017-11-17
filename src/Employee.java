289265f528c8fe496cf0dc91f94e6c8cbce374df
张鑫 <12190922@qq.com>
2017-11-17 14:07:31 +0800

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+    String name;
+    int age;
+    String designation;
+    double salary;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public void empAge(int empAge) {
+        age = empAge;
+    }
+
+    public void empDesignation(String empDesig) {
+        designation = empDesig;
+    }
+
+    /* 设置salary的值*/
+    public void empSalary(double empSalary) {
+        salary = empSalary;
+    }
+
+    public void printEmployee() {
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("Designation:" + designation);
+        System.out.println("Salary:" + salary);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /* 使用构造器创建两个对象 */
+        Employee empOne = new Employee("James Smith");
+        Employee empTwo = new Employee("Mary Anne");
+
+        // 调用这两个对象的成员方法
+        empOne.empAge(26);
+        empOne.empDesignation("Senior Software Engineer");
+        empOne.empSalary(1000);
+        empOne.printEmployee();
+
+        empTwo.empAge(21);
+        empTwo.empDesignation("Software Engineer");
+        empTwo.empSalary(500);
+        empTwo.printEmployee();
+    }
+
+}
+
+
